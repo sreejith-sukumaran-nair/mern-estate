08291e08694a708cb665b9aab3ef9efc1c474ead
Sreejith Sukumaran Nair <sreejith1931991@gmail.com>
2024-09-06 20:28:37 +0530

    created pages and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<><Home/></>} />
+          <Route path="/sign-in" element = {<><SignIn/></>} />
+          <Route path="/sign-up" element = {<><SignUp/></>} />
+          <Route path="/about" element = {<><About/></>} />
+          <Route path="/profile" element = {<><Profile/></>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>Signout</div>
+  )
+}
+
+export default SignUp

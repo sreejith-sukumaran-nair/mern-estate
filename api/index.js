f101739c730a83fa3f4f5b4e7d5b1339bf770c8b
Sreejith Sukumaran Nair <sreejith1931991@gmail.com>
2024-09-07 11:29:00 +0530

    create a function and middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -15,7 +15,18 @@ app.use(express.json())
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+// middleware to handle the errors in proper way
 
+app.use((err,req,res,next) => {
+  const statusCode = err.statusCode || 500 ; // 500 for internal server
+  const message = err.message || "Internal server error";
+
+  return res.status(statusCode).json({
+    success : false ,
+    message ,
+    statusCode ,
+  })
+})
 
 
 app.listen(3000,() => {

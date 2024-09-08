2bc526a33ee69c1aa0a1a24dd796c7556aa57bad
Sreejith Sukumaran Nair <sreejith1931991@gmail.com>
2024-09-08 16:03:51 +0530

    update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -20,7 +22,7 @@ function Header() {
           />
           <CiSearch className="text-slate-700" />
         </form>
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 items-center">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
@@ -31,10 +33,12 @@ function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="text-slate-700 hover:underline">
-              Sign In
-            </li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img className="rounded-full my-auto h-7 w-7 object-cover" src={currentUser.avatar} alt="avatar" />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
